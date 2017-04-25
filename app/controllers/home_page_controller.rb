2e7dd6dd097460e9dbc766dab8381f4c817bba11
Minh Nguyen <ng.huyminh91@gmail.com>
2017-04-25 23:20:49 +0700

    Add initial marker

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -2,4 +2,8 @@ class HomePageController < ApplicationController
   def index
     @photo_shops = PhotoShop.all
   end
+
+  def data
+    @photo_shops = PhotoShop.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   
   resources :photo_shops
   root 'home_page#index'
+  get '/data' => 'home_page#data'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
